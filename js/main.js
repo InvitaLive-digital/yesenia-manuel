0563bdbc4481faaf5d88648ef2d182adca9c0321
JUDITH QUIÑONES INGA <judithcristinaqi@gmail.com>
2025-08-21 18:45:58 -0500

    galeria de fotos

--- js/main.js
+++ js/main.js
@@ -77,10 +77,10 @@
         ],
         responsive: {
             0: { items: 2 },
-            576: { items: 2 },
-            768: { items: 3 },
-            992: { items: 4 },
-            1200: { items: 5 }
+            576: { items: 3 },
+            768: { items: 4 },
+            992: { items: 5 },
+            1200: { items: 6 }
         }
     });
 })(jQuery);
